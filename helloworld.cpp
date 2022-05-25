5485fa062f7baced118c49ddc88167dea76fcdea
Akshay_Shriwaskar <35056470+akshayshriwaskar@users.noreply.github.com>
2022-05-26 00:13:02 +0530

    Example: C++ Hello world program to simply print "Hello World" on computer screen.

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+//Example: C++ Hello world program to simply print "Hello World" on computer screen.
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    cout<<"hello world!";
+    return 0;
+}
